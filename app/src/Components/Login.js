beee013ef888fdef55470c6c549346e717ee614c
Remi <rwisni736@gmail.com>
2021-05-24 20:47:21 -0500

    NOTES: rfc - create react template

--- app/src/Components/Login.js
+++ app/src/Components/Login.js
@@ -5,18 +5,12 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=7261c60af4fc4
 
 export default function Login() {
     return (
-        <Container>
-            <a className="btn btn-success btn-lg" href={AUTH_URL}> Login with Spotify </a>
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh"}}
+        >
+            <a className="btn btn-success btn-lg" href={AUTH_URL}> Test with Spotify </a>
         </Container>
     )
 }
-
-{/*
-rfc - react template
-
-Login page
-
-*/}
 
 
 
